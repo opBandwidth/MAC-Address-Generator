140ae88621b66e633a5a61855be111602bc6608f
Bandwidth <76101857+opBandwidth@users.noreply.github.com>
2024-07-26 16:46:16 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
-return ("XX:XX:XX:XX:XX:XX"):gsub("X", function()
+math.randomseed(os.time() + os.clock() * 1000) -- generates a unique seed for randomness
+return (("XX:XX:XX:XX:XX:XX"):gsub("X", function()
     local randomIDX = math.random(16)
     return ("0123456789ABCDEF"):sub(randomIDX, randomIDX)
-end)
+end))
